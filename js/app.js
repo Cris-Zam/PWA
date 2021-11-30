9d3b2f79b823db1249310596cac16cb050698cf0
Cris Zam <95267152+Cris-Zam@users.noreply.github.com>
2021-11-30 09:10:40 -0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
 	window.addEventListener('load', () => {
-		navigator.serviceWorker.register('../sw.js').then(() => {
+		navigator.serviceWorker.register('../SW.js').then(() => {
 			console.log('Soy el Service Worker registrado y funcionando.')
 		})
 	})
